66f176c8164429fd8d1724e9973f54997b196f30
Aman Ajaz <139737746+Iamanajaz@users.noreply.github.com>
2024-12-12 01:16:00 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,20 @@
+// console.log("aman");
+// document.getElementById("aa").innerHTML="welcome to my world";
+
+// let a=5,b=6;
+// document.getElementById("bb").innerHTML=a+b;
+// console.log("sum of a and b is ",a+b);
+
+// let c = prompt("enter any no:");
+// let d = parseInt(c);
+// document.getElementById('cc').innerHTML= d;
+
+// let a1= prompt("enter first no:");
+// let a2= prompt("enter second no:");
+// document.getElementById('dd').innerHTML= a1+a2;
+
+let b1= prompt("enter first no:");
+let b2= prompt("enter second no:");
+let b3= prompt("enter third no:");
+let e = parseInt(b1) * parseInt(b2) * parseInt(b3);
+document.getElementById('ee').innerHTML= e;
